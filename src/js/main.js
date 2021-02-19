d74aa15d555566546004ce9359c8a040a78a8e0e
Erick Aguilar <ericksvaguilar@gmail.com>
2021-02-19 11:34:19 -0300

    finish js interactivity

---
+++ src/js/main.js
@@ -0,0 +1,39 @@
+const url = 'https://api.jikan.moe/v3/top/anime'
+
+const handleError = error => console.error(error)
+
+const handleSucess = ({ data: { top: animes } }) => {
+  const animeList = document.querySelector('[data-js="anime-list"]')
+  animeList.innerHTML = convertAnimesToLiElements(animes)
+}
+
+const convertAnimesToLiElements = animes => {
+  return animes.reduce((acc, anime) => {
+    return (acc += `
+      <li class="anime-list__item">
+        <figure class="anime-list__figure">
+          <img
+            class="anime-list__img"
+            src="${anime.image_url}"
+            alt="${anime.title} Image"
+            width="225px"
+            height="350px"
+          />
+        </figure>
+        <div class="anime-list__description">
+          <h2 class="anime-list__title">
+            ${anime.rank}. ${anime.title}
+          </h2>
+          <div class="anime__list-text">
+            <p class="anime-list__score">Score: ${anime.score.toFixed(2)}</p>
+            <p>${anime.type} - (${anime.episodes} eps)</p>
+            <p>${anime.start_date} - ${anime.end_date ? anime.end_date : ''}</p>
+            <p>Members: ${anime.members.toLocaleString('pt-BR')}</p>
+          </div>
+        </div>
+      </li>
+    `)
+  }, '')
+}
+
+axios(url).then(handleSucess).catch(handleError)
