88cc09364ce6d8cb3d749038f485f3a392b6c255
Erick Aguilar <ericksvaguilar@gmail.com>
2021-02-19 15:03:26 -0300

    refactor html generated

--- src/js/main.js
+++ src/js/main.js
@@ -11,26 +11,23 @@ const convertAnimesToLiElements = animes => {
   return animes.reduce((acc, anime) => {
     return (acc += `
       <li class="anime-list__item">
-        <figure class="anime-list__figure">
-          <img
-            class="anime-list__img"
-            src="${anime.image_url}"
-            alt="${anime.title} Image"
-            width="225px"
-            height="350px"
-          />
-        </figure>
-        <div class="anime-list__description">
-          <h2 class="anime-list__title">
-            ${anime.rank}. ${anime.title}
-          </h2>
-          <div class="anime__list-text">
-            <p class="anime-list__score">Score: ${anime.score.toFixed(2)}</p>
-            <p>${anime.type} - (${anime.episodes} eps)</p>
-            <p>${anime.start_date} - ${anime.end_date ? anime.end_date : ''}</p>
-            <p>Members: ${anime.members.toLocaleString('pt-BR')}</p>
-          </div>
+        <img
+          class="anime-list__img"
+          src="${anime.image_url}"
+          alt="${anime.title} Image"
+          width="225px"
+          height="350px"
+        />
+        <h2 class="anime-list__title">
+          ${anime.rank}. ${anime.title}
+        </h2>
+        <div class="anime-list__text">
+          <p class="anime-list__score">Score: ${anime.score.toFixed(2)}</p>
+          <p>${anime.type} - (${anime.episodes} eps)</p>
+          <p>${anime.start_date} - ${anime.end_date ? anime.end_date : ''}</p>
+          <p>Members: ${anime.members.toLocaleString('pt-BR')}</p>
         </div>
+        
       </li>
     `)
   }, '')
